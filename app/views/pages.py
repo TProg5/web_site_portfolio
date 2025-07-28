d2a0cd150b05d6edbf155d3c79b06d293862f28b
TProg5 <ohotnikovtimur5@gmail.com>
2025-07-28 21:42:45 +0300

    html update

--- app/views/pages.py
+++ app/views/pages.py
@@ -2,15 +2,24 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 
-from app.core.templates import templates
+from app.core.templates import templates, get_translator
 
-router = APIRouter()
+router = APIRouter(tags=["Pages"])
 
 
 @router.route("/")
-@router.get("/main", response_class=HTMLResponse)
+@router.get("/main", response_class=HTMLResponse, tags=["Pages"])
 async def main_page(request: Request):
+    lang = request.state.lang
+    translator = get_translator(lang=lang)
+    
+    _ = translator.gettext
+
     return templates.TemplateResponse(
-        request=request, 
-        name="index.html"
+        request=request,
+        name="index.html",
+        context={
+            "request": request,
+            "_": _
+        }
     )
